47be4cb64aa69f1ec125630f23ccb695aa86cf76
Isabela Gaiteiro <97116169+Isagaiteiro@users.noreply.github.com>
2022-06-18 18:24:42 -0300

    Update main.cpp

--- Algoritimos e Lógica de Programação/Lista de exercicios/lista ex11/main.cpp
+++ Algoritimos e Lógica de Programação/Lista de exercicios/lista ex11/main.cpp
@@ -9,18 +9,18 @@ int main(int argc, char** argv){
     int a[10][10], linha=0, coluna=0, valor=0;
     int maior=a[0][0];
 
-    for(int i=0; i<9; i++){
-        for(int j=0; j<9; j++){
+    for(int i=0; i<=9; i++){
+        for(int j=0; j<=9; j++){
             cout<<"Digite um número\n";
             cin>>a[i][j];
             }
     }
     
-    for (int i=0; i<9; i++){
+    for (int i=0; i<=9; i++){
         cout<< a[i][0]<<"|"<<a[i][1]<<"|"<<a[i][2]<<"|"<<a[i][3]<<"|"<<a[i][4]<<"|"<<a[i][5]<<"|"<<a[i][6]<<"|"<<a[i][7]<<"|"<<a[i][8]<<"|"<<a[i][9]<<"|"<<endl;
     }
-    for(int i=0; i<9; i++){
-        for(int j=0; j<9; j++){
+    for(int i=0; i<=9; i++){
+        for(int j=0; j<=9; j++){
             if( a[i][j] > maior ){
             maior=a[i][j];
             linha = i;
